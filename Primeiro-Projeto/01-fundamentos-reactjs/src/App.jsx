c6226ec6ae04b935747b6c67fce03bd037288322
VitorMiguel <vitor.miguel0581@gmail.com>
2023-03-24 17:00:38 -0300

    add all

--- Primeiro-Projeto/01-fundamentos-reactjs/src/App.jsx
+++ Primeiro-Projeto/01-fundamentos-reactjs/src/App.jsx
@@ -1,15 +1,24 @@
-import Post from './Post';
+import { Post } from './components/Post';
 import { Header } from './components/Header';
 
-import './style.css';
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
 	return (
-		<diiv>
+		<div>
 			<Header />
-			<Post author="Vitor" content="Texto texto" />
-			<Post author="João" content="Aii uii" />
-		</diiv>
+
+			<div className={styles.wrapper}>
+				<Sidebar />
+				<main>
+					<Post />
+					<Post />
+				</main>
+			</div>
+		</div>
 	);
 }
 
---
+++ Primeiro-Projeto/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images.unsplash.com/photo-1536104968055-4d61aa56f46a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+			/>
+			<div className={styles.profile}>
+				<img src="https://github.com/vitorsat.png" className={styles.avatar} />
+
+				<strong>Vitor Miguel</strong>
+				<span>Web Developer</span>
+			</div>
+
+			<footer>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	);
+}
