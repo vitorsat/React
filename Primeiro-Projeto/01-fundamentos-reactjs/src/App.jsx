d2d6aa79f20fb2dde79fe9ecfa3c7979e1e8ff58
VitorMiguel <vitor.miguel0581@gmail.com>
2023-03-27 15:22:56 -0300

    add all

--- Primeiro-Projeto/01-fundamentos-reactjs/src/App.jsx
+++ Primeiro-Projeto/01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,45 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from './components/Sidebar';
 
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: 'https://avatars.githubusercontent.com/u/61619897?v=4',
+			name: 'Vitor Miguel',
+			role: 'Web Developer',
+		},
+		content: [
+			{ type: 'paragraph', content: 'Fala galeraa 👋' },
+			{
+				type: 'paragraph',
+				content:
+					'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+			},
+			{ type: 'link', content: '👉 jane.design/doctorcare ' },
+		],
+		publishedAt: new Date('2021-05-11 08:13:15'),
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: 'https://github.com/vitorsat.png',
+			name: 'João Miguel',
+			role: 'Web Developer',
+		},
+		content: [
+			{ type: 'paragraph', content: 'Fala galeraa 👋' },
+			{
+				type: 'paragraph',
+				content:
+					'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+			},
+			{ type: 'link', content: '👉 jane.design/doctorcare ' },
+		],
+		publishedAt: new Date('2021-05-14 23:13:15'),
+	},
+];
+
 function App() {
 	return (
 		<div>
@@ -14,8 +53,15 @@ function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post />
-					<Post />
+					{posts.map((post) => {
+						return (
+							<Post
+								author={post.author}
+								content={post.content}
+								publishedAt={post.publishedAt}
+							/>
+						);
+					})}
 				</main>
 			</div>
 		</div>
