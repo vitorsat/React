1e4a7dc65cdc3a2e54ccbeb2ef63b7c7d1ec3e32
VitorMiguel <vitor.miguel0581@gmail.com>
2023-04-17 15:18:44 -0300

    feat: add all

--- Primeiro-Desafio/to-do/src/components/Check.tsx
+++ Primeiro-Desafio/to-do/src/components/Check.tsx
@@ -4,7 +4,7 @@ export function Check() {
 	return (
 		<div className={styles.round}>
 			<input type="checkbox" className={styles.input} id="checkbox" />
-			<label htmlFor="checkbox"></label>
+			<label htmlFor="checkbox" className={styles.label}></label>
 		</div>
 	);
 }
