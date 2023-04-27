d8e5c2dc6acfd6d28987329409b0dde2940820ed
VitorMiguel <vitor.miguel0581@gmail.com>
2023-04-27 17:29:56 -0300

    feat: add theme

--- Segundo-Projeto/02-ignite-timer/src/components/Button.styles.ts
+++ Segundo-Projeto/02-ignite-timer/src/components/Button.styles.ts
@@ -17,8 +17,10 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 	width: 100px;
 	height: 40px;
 
-	${(props) =>
+	background-color: ${(props) => props.theme.primary};
+
+	/* ${(props) =>
 		css`
 			background-color: ${buttonVariants[props.variant]};
-		`}
+		`} */
 `;
