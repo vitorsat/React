e184efba6ea3d9f6e7363a39dcf372027e60c5db
VitorMiguel <vitor.miguel0581@gmail.com>
2023-05-15 14:17:21 -0300

    feat: add home

---
+++ Segundo-Projeto/02-ignite-timer/src/pages/home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={25} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
