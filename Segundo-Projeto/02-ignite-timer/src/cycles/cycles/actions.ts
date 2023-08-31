ca315613d71edff51f8ab5ca41b1172aedc93614
VitorMiguel <vitor.miguel0581@gmail.com>
2023-08-31 20:12:10 -0300

    add all

--- Segundo-Projeto/02-ignite-timer/src/cycles/cycles/actions.ts
+++ Segundo-Projeto/02-ignite-timer/src/cycles/cycles/actions.ts
@@ -1,9 +1,9 @@
-import { Cycle } from './cycles'
+import { Cycle } from "./reducer";
 
 export enum ActionTypes {
-  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
-  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
-  MARK_CURRENT_CYCLES_AS_FINISHED = 'MARK_CURRENT_CYCLES_AS_FINISHED',
+  ADD_NEW_CYCLE = "ADD_NEW_CYCLE",
+  INTERRUPT_CURRENT_CYCLE = "INTERRUPT_CURRENT_CYCLE",
+  MARK_CURRENT_CYCLES_AS_FINISHED = "MARK_CURRENT_CYCLES_AS_FINISHED",
 }
 
 export function addNewCycleAction(newCycle: Cycle) {
@@ -12,17 +12,17 @@ export function addNewCycleAction(newCycle: Cycle) {
     payload: {
       newCycle,
     },
-  }
+  };
 }
 
 export function markCurrentCycleAsFinishedAction() {
   return {
     type: ActionTypes.MARK_CURRENT_CYCLES_AS_FINISHED,
-  }
+  };
 }
 
 export function interruptCurrentCycleAction() {
   return {
     type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-  }
+  };
 }
